d5ecb666b9a2de98d9682f185a045ae3d536c9ce
Boris Karabut <boriskarabytwork@gmail.com>
2023-04-10 10:50:13 +0300

    Фундамент проекта

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <HeaderComp/>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import HeaderComp from '@/widgets/shared/HeaderComp.vue';
+
+  export default defineComponent({
+    name: 'HomePage'
+  })
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/widgets/shared/HeaderComp.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>FireWork</h1>
+    <nav>
+      <router-link to="/">
+        Заказчику
+      </router-link>
+      <router-link to="/">
+        Исполнителю
+      </router-link>
+      <button>
+        Вход
+      </button>
+      <button>
+        Регистрация
+      </button>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'HeaderComp'
+  })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
